8855a5a41d07e8375e9ed9ececc3280b8340a544
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-03-10 12:20:01 +0000

    added remove token on logout

--- frontend/web-app/src/components/Homepage/Homepage.js
+++ frontend/web-app/src/components/Homepage/Homepage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from 'react-redux'
 
-import { getAccessToken, setAccessToken } from '@components/authentication'
+import { getAccessToken, removeAccessToken } from '@components/authentication'
 import Button from '@components/Button';
 import { SignupForm, LoginForm } from '@components/forms';
 import { CoupledModal, Modal } from '@components/Modals';
@@ -11,14 +11,13 @@ import { SignupOrLogin } from '@components/SignupOrLogin/SignupOrLogin'
 const Homepage = () => {
   const loggedIn = useSelector(selectLogin)
   const dispatch = useDispatch()
-
+  console.log(loggedIn)
   return (
     <div>
       {getAccessToken()}
       <h1>DND PLUS</h1>
-
       {loggedIn
-        ? <div onClick={() => { setAccessToken(null); dispatch(set(false)) }}>
+        ? <div onClick={() => { removeAccessToken(); dispatch(set(false)) }}>
           log out
         </div>
         : <SignupOrLogin />
--- frontend/web-app/src/components/authentication/index.js
+++ frontend/web-app/src/components/authentication/index.js
@@ -1,3 +1,4 @@
 export const getAccessToken =()=>localStorage.getItem('token')
 
 export const setAccessToken =(token)=>{localStorage.setItem('token',token)}
+export const removeAccessToken =()=>{localStorage.removeItem('token')}
