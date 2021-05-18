f5c98301fe31f9f9797e829e5b2a88415e052b93
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-05-18 19:24:23 +0100

    Added mutation, created tests

--- backend/internalapi/mutation.py
+++ backend/internalapi/mutation.py
@@ -3,9 +3,10 @@ import graphene
 from .mutations.user.create_user import CreateUser
 from .mutations.user.authenticate_user import AuthenticateUser
 from .mutations.user.is_authenticated import IsAuthenticated
-
+from .mutations.campaign.create_campaign import CreateCampaign
 
 class RootMutation(graphene.ObjectType):
     create_user = CreateUser.Field()
     authenticate_user = AuthenticateUser.Field()
     is_authenticated = IsAuthenticated.Field()
+    create_campaign = CreateCampaign.Field()
---
+++ backend/tests/internalapi/mutations/test_create_campaign.py
@@ -0,0 +1,69 @@
+import pytest
+
+from django.core import mail
+
+from campaign.models import Campaign as DjangoCampaign
+from tests.user.factories import UserFactory
+from tests.campaign.factories import CampaignFactory
+from user.models import User as DjangoUser
+
+
+@pytest.mark.django_db
+class TestCreateCampaignMutation:
+    mutation = """
+        mutation createCampaignMutation($input: CreateCampaignInput!) {
+            createCampaign(input: $input) {
+                __typename
+                ... on CreateCampaignSuccess {
+                    campaign {
+                        title
+                        roomCode
+                    }
+                }
+                ... on CreateCampaignError {
+                    nonFieldErrors
+                    fieldErrors {
+                        fieldName
+                        errors
+                    }
+                }
+            }
+        }
+    """
+
+
+    @pytest.fixture
+    def user(self):
+        return UserFactory(email="testymctestface@flatmatch.com")
+
+    @pytest.fixture
+    def client(self, user, graphql_client_factory):
+        return graphql_client_factory(user=user)
+
+    @pytest.fixture
+    def variable_values(self):
+        return {
+            "input": {
+                "title": "My sick campaign",
+            }
+        }
+
+    def test_create_campaign_success(self, variable_values, client):
+        assert DjangoCampaign.objects.count() == 0
+        response = client.execute(self.mutation, variable_values=variable_values)
+        result = response["data"]["createCampaign"]
+
+        assert result["__typename"] == "CreateCampaignSuccess"
+        campaign_response = result["campaign"]
+        assert campaign_response["title"] =="My sick campaign"
+        assert DjangoCampaign.objects.count() == 1
+
+    def test_create_campaign_fail(self, variable_values, client):
+        variable_values['input']['title']="This title is too long too be accepted"
+        assert DjangoCampaign.objects.count() == 0
+        response = client.execute(self.mutation, variable_values=variable_values)
+        result = response["data"]["createCampaign"]
+
+        assert result["__typename"] == "CreateCampaignError"
+        assert result["fieldErrors"][0]["fieldName"] =="TITLE"
+        assert DjangoCampaign.objects.count() == 0
