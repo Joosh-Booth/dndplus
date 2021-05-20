7b2d49e2a705b233c8d0daa64f1abbeeb4208a03
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-05-20 18:04:58 +0100

    Changed H1 size

--- frontend/web-app/src/components/headers/H1.js
+++ frontend/web-app/src/components/headers/H1.js
@@ -2,6 +2,4 @@ import styled from "@emotion/styled";
 
 import { baseText } from "@dnd/theme";
 
-export const H1 = styled.h1(baseText, { fontWeight:'bold',fontSize:30, });
-
-//export default H1;
+export const H1 = styled.h1(baseText, { fontWeight:'bold',fontSize:40, });
