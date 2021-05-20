82adea223dbd8353c33c708378ca206115c261f1
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-05-20 18:08:36 +0100

    Created H3, H4, Added to index

---
+++ frontend/web-app/src/components/headers/H3.js
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled";
+
+import { baseText } from "@dnd/theme";
+
+export const H3 = styled.h3(baseText, { fontWeight:'bold',fontSize:30, });
---
+++ frontend/web-app/src/components/headers/H4.js
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled";
+
+import { baseText } from "@dnd/theme";
+
+export const H4 = styled.h4(baseText, { fontWeight:'bold',fontSize:25, });
