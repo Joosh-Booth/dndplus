083f0a7c80024c57b7483662f0103a831e6138da
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-05-20 17:49:02 +0100

    Create Campaign page started

--- frontend/web-app/src/pages/CreateCampaign.js
+++ frontend/web-app/src/pages/CreateCampaign.js
@@ -1,11 +1,18 @@
 import Helmet from 'react-helmet'
-
+import { VerticalFlexContainer, HorizontalFlexContainer, CenteredContainer } from '@components/containers'
+import { TextInput } from '@components/inputs'
+import Text from '@components/Text'
 
 const CreateCampaign = () =>{
   return (
     <>
       <Helmet title="DNDPlus | Create game"/>
-      CREATE CAMPAIGN
+      <VerticalFlexContainer>
+        <HorizontalFlexContainer css={{margin:40, textAlign:'center'}}>
+         <div css={{ margin:'auto 0 auto 0' }}><Text >Choose a title for your campaign: </Text></div>
+         <TextInput css={{borderStyle:'none', height:60, fontSize:45, width:'30%'}}/>
+        </HorizontalFlexContainer>
+      </VerticalFlexContainer>
     </>
   )
 };
