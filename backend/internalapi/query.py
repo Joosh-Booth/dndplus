0c69cf5a812cd59a96a24679af854bfce42c7634
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-07-19 18:50:24 +0100

    created new query and updated schema

--- backend/internalapi/query.py
+++ backend/internalapi/query.py
@@ -6,8 +6,13 @@ from .definitions.user import User
 class RootQuery(graphene.ObjectType):
 
     user_by_reference = graphene.Field(User, required=False, reference=graphene.String(required=True))
+    is_allowed_on_page = graphene.Boolean(required=True,page=graphene.String(required=True))
 
 
     def resolve_user_by_reference(root, info, reference):
-
       return DjangoUser.objects.filter(reference=reference).first()
+
+    def resolve_is_allowed_on_page(root, info, page):
+      print(page)
+      print(info.context)
+      return True
