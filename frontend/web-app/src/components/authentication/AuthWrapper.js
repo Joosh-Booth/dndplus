422151848ed567843862fd09ee4ab3d66e72127c
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-07-20 16:31:09 +0100

    Changed fetch policy to call on every component load

--- frontend/web-app/src/components/authentication/AuthWrapper.js
+++ frontend/web-app/src/components/authentication/AuthWrapper.js
@@ -1,5 +1,5 @@
 
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery, graphql } from "@apollo/client";
 
 //Takes in page requested to view. Run query with page and user credentials to see if user is allowed on page. If success show children. 
 //If not show "Authentication failed. Please ensure you are logged in and allowed to view this page."
@@ -13,15 +13,16 @@ const IS_ALLOWED_ON_PAGE = gql`
 
 const AuthWrapper = ({ page, params }) => {
   const { loading, error, data } = useQuery(IS_ALLOWED_ON_PAGE, {
+    fetchPolicy: "network-only",
     variables: {
       input: {
         page: page,
         params: params
       }
     }
-  })
+  });
 
-  if (loading) return <div>Loading</div>
+  if (loading) {console.log("laoding"); return <div>Loading</div>}
 
   return (
     <div>
