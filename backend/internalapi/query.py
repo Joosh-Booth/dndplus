ddc0fd5477b1d2bfdb7e59fcbf1abed86a0207f4
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-07-22 10:31:53 +0100

    campaign by reerence query

--- backend/internalapi/query.py
+++ backend/internalapi/query.py
@@ -1,6 +1,9 @@
+from campaign.models import Campaign
 import graphene
 
+from campaign.models import Campaign as DjangoCampaign
 from user.models import User as DjangoUser
+from .definitions.campaign import Campaign
 from .definitions.user import User
 from .definitions.authentication import PageInput
 
@@ -8,7 +11,7 @@ class RootQuery(graphene.ObjectType):
 
     user_by_reference = graphene.Field(User, required=False, reference=graphene.String(required=True))
     is_allowed_on_page = graphene.Boolean(required=True, page=PageInput(required=True,name="input"))
-
+    campaign_by_reference = graphene.Field(Campaign, required=True, reference=graphene.String(required=True))
 
     def resolve_user_by_reference(root, info, reference):
       return DjangoUser.objects.filter(reference=reference).first()
@@ -18,3 +21,7 @@ class RootQuery(graphene.ObjectType):
       # get contexr.use.id and check if theyre allowed on page. If page is campaign get campaign reference.
       # 
       return True
+    
+    def resolve_campaign_by_reference(root, info, reference):
+      return DjangoCampaign.objects.filter(reference=reference).first()
+      
