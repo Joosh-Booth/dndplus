737fafbc4a436c82b04847592a26f99b14b98cac
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-08-01 13:57:05 +0100

    Headers size updated

--- frontend/web-app/src/components/headers/H3.js
+++ frontend/web-app/src/components/headers/H3.js
@@ -2,4 +2,4 @@ import styled from "@emotion/styled";
 
 import { baseText } from "@dnd/theme";
 
-export const H3 = styled.h3(baseText, { fontWeight:'bold',fontSize:'2.25vw', lineHeight:1.25});
+export const H3 = styled.h3(baseText, { fontWeight:'bold',fontSize:'2vw', lineHeight:1.25});
--- frontend/web-app/src/components/headers/H4.js
+++ frontend/web-app/src/components/headers/H4.js
@@ -2,4 +2,4 @@ import styled from "@emotion/styled";
 
 import { baseText } from "@dnd/theme";
 
-export const H4 = styled.h4(baseText, { fontWeight:'bold',fontSize:'2.125vw', lineHeight:1});
+export const H4 = styled.h4(baseText, { fontWeight:'bold',fontSize:'1.5vw', lineHeight:1});
