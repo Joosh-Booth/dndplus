c044e55c90c4a06f61f41e71e230049f39f1f823
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-08-01 13:51:18 +0100

    Headers based on vw

--- frontend/web-app/src/components/headers/H1.js
+++ frontend/web-app/src/components/headers/H1.js
@@ -2,4 +2,4 @@ import styled from "@emotion/styled";
 
 import { baseText } from "@dnd/theme";
 
-export const H1 = styled.h1(baseText, { fontWeight:'bold',fontSize:40, });
+export const H1 = styled.h1(baseText, { fontWeight:'bold',fontSize:'2.5vw', lineHeight:2});
--- frontend/web-app/src/components/headers/H2.js
+++ frontend/web-app/src/components/headers/H2.js
@@ -2,4 +2,4 @@ import styled from "@emotion/styled";
 
 import { baseText } from "@dnd/theme";
 
-export const H2 = styled.h2(baseText, { fontWeight:'bold',fontSize:35, });
+export const H2 = styled.h2(baseText, { fontWeight:'bold',fontSize:'2.375vw', lineHeight:1.5});
