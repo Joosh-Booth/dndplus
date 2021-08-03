68c369aff3c18631a492ff6770b85bee666232d4
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-08-03 21:50:17 +0100

    check if user is allowed on campaign page query

--- backend/internalapi/query.py
+++ backend/internalapi/query.py
@@ -12,7 +12,7 @@ class RootQuery(graphene.ObjectType):
 
     user_by_reference = graphene.Field(User, required=False, reference=graphene.String(required=True))
     is_allowed_on_page = graphene.Boolean(required=True, page=PageInput(required=True,name="input"))
-    campaign_by_reference = graphene.Field(Campaign, required=True, reference=graphene.String(required=True))
+    campaign_by_reference = graphene.Field(Campaign, required=False, reference=graphene.String(required=True))
     campaign_by_user = graphene.List(Campaign, required=True)
     invitations_by_user = graphene.List(Invitation, required=True)
 
@@ -21,14 +21,20 @@ class RootQuery(graphene.ObjectType):
 
     def resolve_is_allowed_on_page(root, info, page):
       # get contexr.use.id and check if theyre allowed on page. If page is campaign get campaign reference.
-      #
-      return True
+      return page_authentication(page, info.context.user)
 
     def resolve_campaign_by_reference(root, info, reference):
-      return DjangoCampaign.objects.filter(reference=reference).first()
+      return DjangoCampaign.objects.filter(room_code=reference).first()
 
     def resolve_campaign_by_user(root, info):
       return info.context.user.campaigns.all()
 
     def resolve_invitation_by_user(root, info):
       return info.context.user.invitations.all()
+
+def page_authentication(page, user):
+    print(page.params)
+    if page.page == "campaign":
+        if user.campaigns.filter(room_code=page.params).exists():
+            return True
+    return False  
