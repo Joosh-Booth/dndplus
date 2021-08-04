44127b2085d2a7fc9ed7e70381e735c3f53f2e1f
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-08-04 11:26:56 +0100

    H2 size

--- frontend/web-app/src/components/headers/H2.js
+++ frontend/web-app/src/components/headers/H2.js
@@ -2,4 +2,4 @@ import styled from "@emotion/styled";
 
 import { baseText } from "@dnd/theme";
 
-export const H2 = styled.h2(baseText, { fontWeight:'bold',fontSize:'2.5vw', lineHeight:1.5});
+export const H2 = styled.h2(baseText, { fontWeight:'bold',fontSize:'2.75vw', lineHeight:1.5});
