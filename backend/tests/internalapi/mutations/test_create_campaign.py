35fe307c141113ac712a4c28dec8f7daf968dd66
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-08-18 13:21:57 +0100

    fixed failed test

--- backend/tests/internalapi/mutations/test_create_campaign.py
+++ backend/tests/internalapi/mutations/test_create_campaign.py
@@ -61,7 +61,7 @@ class TestCreateCampaignMutation:
         campaign_response = result["campaign"]
         assert campaign_response["title"] =="My sick campaign"
         print(campaign_response["createdBy"])
-        assert DjangoCampaign.objects.count() == 0
+        assert DjangoCampaign.objects.count() == 1
 
     def test_create_campaign_fail(self, variable_values, client):
         variable_values['input']['title']="This title is too long too be accepted"
