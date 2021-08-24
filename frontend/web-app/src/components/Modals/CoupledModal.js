3390de452b7e041b555e184f4c8e8ea547f2e88b
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-08-24 15:16:48 +0100

    coupled modal changed to allow passed onclick functionality

--- frontend/web-app/src/components/Modals/CoupledModal.js
+++ frontend/web-app/src/components/Modals/CoupledModal.js
@@ -3,18 +3,21 @@ import React, { useState } from "react";
 export default function CoupledModal({
   element: Element,
   modalElement: Modal,
-  modalOpenState,
+  modalOpenState=null,
+  onClose=()=>null,
   ...props
 }) {
-  const [open, setOpen] = useState(modalOpenState);
-  function handleOpen() {
+  const [open, setOpen] = useState(modalOpenState||false);
+  const handleOpen = () => {
     setOpen(true);
+    Element.props.onClick&&Element.props.onClick();
   }
-  function handleClose() {
+  const handleClose = () => {
     setOpen(false);
+    onClose()
   }
   return (
-    <>
+    <div>
       {React.isValidElement(Element) &&
         React.cloneElement(Element, {
           onClick: handleOpen,
@@ -22,10 +25,10 @@ export default function CoupledModal({
 
       {React.isValidElement(Modal) &&
         React.cloneElement(Modal, {
-          open: open,
+          open: modalOpenState!=null?modalOpenState:open,
           onCloseHandler: handleClose,
           ...props,
         })}
-    </>
+    </div>
   );
 }
