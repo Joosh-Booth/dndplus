8bb6ffa8af20fb593c99059f131eb49d376c04e4
Joosh-Booth <jjoshuabooth@hotmail.com>
2021-10-17 14:51:45 +0100

    Apollo added to storybook

--- frontend/web-app/.storybook/preview.js
+++ frontend/web-app/.storybook/preview.js
@@ -1,4 +1,7 @@
-
+import { MockedProvider } from '@apollo/client/testing'; // Use for Apollo Version 3+
 export const parameters = {
+  apolloClient: {
+    MockedProvider,
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
